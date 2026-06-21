f06058cba50c1eb8393998703818c03810a6636a
shilpa-shinde <70946490+shilpa-shinde@users.noreply.github.com>
2026-06-21 14:20:48 +0530

    Add files via upload
    day 1  Assigment

---
+++ datatypes.ts
@@ -0,0 +1,14 @@
+let firstname = "Shilpa"
+console.log(typeof firstname);
+
+let companyName ="EY India"
+console.log(typeof companyName);
+
+let mobileNumber = 9962754955
+console.log(typeof mobileNumber);
+
+let isAutomation = false
+console.log(typeof isAutomation);
+
+let hasPlaywright
+console.log(typeof hasPlaywright);
